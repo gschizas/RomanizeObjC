b505f22cd15bbc2c68da0268eaa9fbcf437282ca
George Schizas <gschizas@gmail.com>
2016-11-06 15:45:25 +0200

    Initial Commit

---
+++ RomanizeObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RomanizeObjC
+//
+//  Created by George Schizas on 06/11/2016.
+//  Copyright © 2016 George Schizas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
